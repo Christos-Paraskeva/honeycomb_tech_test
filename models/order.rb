90600e50bf55415bdaf80c7e1a96ce97bcb9098b
Christos <chris.paraskeva10@gmail.com>
2017-05-09 19:51:00 +0100

    refactors output method

--- models/order.rb
+++ models/order.rb
@@ -5,11 +5,12 @@ class Order
     price: 8
   }.freeze
 
-  attr_accessor :material, :items
+  attr_accessor :material, :items, :result
 
   def initialize(material)
     self.material = material
     self.items = []
+    @result = []
   end
 
   def add(broadcaster, delivery)
@@ -17,30 +18,37 @@ class Order
   end
 
   def total_cost
+    p items
     items.inject(0) { |memo, (_, delivery)| memo += delivery.price }
   end
 
   def output
     [].tap do |result|
       result << "Order for #{material.identifier}:"
-
-      result << COLUMNS.map { |name, width| name.to_s.ljust(width) }.join(' | ')
+      result << columns
       result << output_separator
-
-      items.each_with_index do |(broadcaster, delivery), index|
-        result << [
-          broadcaster.name.ljust(COLUMNS[:broadcaster]),
-          delivery.name.to_s.ljust(COLUMNS[:delivery]),
-          ("$#{delivery.price}").ljust(COLUMNS[:price])
-        ].join(' | ')
-      end
-
+      result << print_information
       result << output_separator
       result << "Total: $#{total_cost}"
     end.join("\n")
   end
 
   private
+
+  def print_information
+    items.each_with_index do |(broadcaster, delivery), index|
+      result << [
+        broadcaster.name.ljust(COLUMNS[:broadcaster]),
+        delivery.name.to_s.ljust(COLUMNS[:delivery]),
+        ("$#{delivery.price}").ljust(COLUMNS[:price])
+      ].join(' | ')
+    end
+    return result
+  end
+
+  def columns
+    COLUMNS.map { |name, width| name.to_s.ljust(width) }.join(' | ')
+  end
 
   def output_separator
     @output_separator ||= COLUMNS.map { |_, width| '-' * width }.join(' | ')
