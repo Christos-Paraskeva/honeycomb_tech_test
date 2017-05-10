0298fa08d8325a4c0fe0fbe2bf7f8a58b555541c
Christos <chris.paraskeva10@gmail.com>
2017-05-10 17:04:46 +0100

    makes small edit to the discount class

--- models/discount.rb
+++ models/discount.rb
@@ -9,12 +9,12 @@ class Discount
   end
 
   def calculate_total_cost(percent_off = 10, total_cost)
-    total_cost -= calculate_percentage_off(total_cost)
+    total_cost -= calculate_percentage_off(total_cost, percent_off)
   end
 
   private
 
-  def calculate_percentage_off(total_cost, percent_off = 10)
+  def calculate_percentage_off(total_cost, percent_off)
     total_cost * (percent_off / 100.round(1))
   end
 
